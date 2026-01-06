5e6bce8dfb57425a0db5b7719affe8ede30f410f
User Guy <decoding.c.t@gmail.com>
2026-01-06 16:10:21 +0200

    explore

--- src/cmps/PostList.jsx
+++ src/cmps/PostList.jsx
@@ -1,6 +1,10 @@
 import { userService } from '../services/user';
 import { PostPreview } from './PostPreview';
 
+// Icons
+import { FaHeart } from "react-icons/fa";
+import { BiSolidMessageRounded } from "react-icons/bi";
+
 export function PostList({ posts, openPost }) {
 	function shouldShowActionBtns(post) {
 		const user = userService.getLoggedinUser();
@@ -10,13 +14,14 @@ export function PostList({ posts, openPost }) {
 		return post.owner?._id === user._id;
 	}
 
-	function openPost() {}
+	function openPost() { }
 
 	return (
 		<section>
 			<div className="post-list-grid">
 				{posts.map((post) => (
 					<div className="post" key={post._id}>
+
 						<PostPreview post={post} openPost={openPost} />
 					</div>
 				))}
--- src/cmps/PostPreview.jsx
+++ src/cmps/PostPreview.jsx
@@ -1,9 +1,23 @@
-import { Link } from 'react-router-dom';
+import { FaHeart } from "react-icons/fa";
+import { BiSolidMessageRounded } from "react-icons/bi";
 
 export function PostPreview({ post, openPost }) {
 	return (
 		<article className="post-preview" onClick={openPost}>
-			<img src={post.imgUrl}></img>
+			<img src={post.imgUrl} alt="post" />
+
+			<div className="post-overlay">
+				<div className="post-stats">
+					<div className="stat">
+						<FaHeart />
+						<span>{post.likesCount?.toLocaleString() || 0}</span>
+					</div>
+					<div className="stat">
+						<BiSolidMessageRounded />
+						<span>{post.commentsCount?.toLocaleString() || 0}</span>
+					</div>
+				</div>
+			</div>
 		</article>
 	);
 }
