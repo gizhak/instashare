2e988222c5bb4496609a9e2aa9624f5626996a08
Lea Guzmicka <lea.guzmicka@outlook.com>
2026-01-07 18:43:44 +0200

    adding _createPosts, fixing the css picture disply in PostList

--- src/pages/PostIndex.jsx
+++ src/pages/PostIndex.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
-import { loadPosts, addPost, updatePost, removePost, addPostComment } from '../store/actions/post.actions';
+import {
+	loadPosts,
+	addPost,
+	updatePost,
+	removePost,
+	addPostComment,
+} from '../store/actions/post.actions';
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service';
 import { postService } from '../services/post';
 import { userService } from '../services/user';
@@ -9,55 +15,12 @@ import { userService } from '../services/user';
 // Icons
 import { IoSearchOutline } from 'react-icons/io5';
 
-
 import { PostList } from '../cmps/PostList';
 //import { PostFilter } from '../cmps/PostFilter';
 
 export function PostIndex() {
 	const [filterBy, setFilterBy] = useState(postService.getDefaultFilter());
-	//const posts = useSelector((storeState) => storeState.postModule.posts);
-
-	const posts = [
-		{
-			_id: 's101',
-			txt: 'Lake trip with the best 🩷',
-			imgUrl:
-				'https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg',
-			likesCount: 8734,
-			commentsCount: 58,
-			by: {
-				_id: 'u101',
-				fullname: 'sunflower_power77',
-				imgUrl: 'http://some-img',
-			},
-		},
-		{
-			_id: 's1012',
-			txt: 'Lake  🩷',
-			imgUrl:
-				'https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg',
-			likesCount: 8734,
-			commentsCount: 58,
-			by: {
-				_id: 'u101',
-				fullname: 'sunflower_power77',
-				imgUrl: 'http://some-img',
-			},
-		},
-		{
-			_id: 's1012',
-			txt: 'Lake  🩷',
-			imgUrl:
-				'https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg',
-			likesCount: 8734,
-			commentsCount: 58,
-			by: {
-				_id: 'u101',
-				fullname: 'sunflower_power77',
-				imgUrl: 'http://some-img',
-			},
-		},
-	];
+	const posts = useSelector((storeState) => storeState.postModule.posts);
 
 	useEffect(() => {
 		loadPosts(filterBy);
@@ -101,15 +64,13 @@ export function PostIndex() {
 	// 	}
 	// }
 
-
-
 	return (
 		<section className="post-index">
 			<header>
 				<div className="search-container">
 					<IoSearchOutline className="search-icon" />
 					<input
-						className='search-bar'
+						className="search-bar"
 						onChange={onSearch}
 						placeholder="Search"
 					/>
